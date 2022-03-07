0d36db58557d6de73674f7200d0b1966c76b1960
Pratik <joshipratik300@gmail.com>
2022-03-07 15:47:01 +0530

    initial commit

---
+++ src/firstpackage/employ.java
@@ -0,0 +1,43 @@
+package firstpackage;
+import java.util.*;
+
+
+public class employ {
+    public static void main(String args[]){
+        String ename ;
+        int id;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your email:-> ");
+        String email = sc.next();
+        int sl= email.length();
+        if(email.indexOf('@')<=0){
+            System.out.println("invalid");
+        }
+        else if (email.indexOf('@') == (sl-1))
+        {
+            System.out.println("invalid");
+        }
+        else
+        {
+            String afterat;
+            afterat=email.substring(email.indexOf('@')+1,sl-1);
+            if(afterat.indexOf('.')<=0){
+                System.out.println("invalid");
+            }
+            else if (afterat.indexOf('.') == (sl-1))
+            {
+                System.out.println("invalid");
+            }
+            else
+            {
+                System.out.println("Valid");
+            }
+
+        }
+
+
+
+    }
+
+
+}
